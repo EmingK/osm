02a0863ff0510643a28e924841b0e0eb9f766911
EmingK <EMKCentric@gmail.com>
2016-05-26 23:33:47 +0800

    initial commit

---
+++ osm_processor/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * 由SharpDevelop创建。
+ * 用户： EmingK
+ * 日期: 2013/8/22
+ * 时间: 15:23
+ * 
+ * 要改变这种模板请点击 工具|选项|代码编写|编辑标准头文件
+ */
+using System;
+
+namespace osm
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			osmProcessor p=new osmProcessor();
+			p.setFile(args[0]);
+			p.writeOutput();
+		}
+	}
+}
